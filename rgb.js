a238b78132d3897b40307ca0434a540e07fc7663
matthova <matthova87@gmail.com>
2016-06-07 05:23:03 +0000

    added script for receiving raw rgb data over osc instead of parsing notes

---
+++ rgb.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const ws281x = require('rpi-ws281x-native');
+const osc = require('node-osc');
+
+const NUM_LEDS = 60;
+const pixelData = new Uint32Array(NUM_LEDS);
+
+let r_destination = 0;
+let g_destination = 0;
+let b_destination = 0;
+
+ws281x.init(NUM_LEDS);
+
+setInterval(() => {
+  write_lights(r_destination, g_destination, b_destination);
+}, 10);
+
+// ---- trap the SIGINT and reset before exit
+process.on('SIGINT', function () {
+  ws281x.reset();
+  process.nextTick(() => {
+    process.exit(0);
+  });
+});
+
+function rgb2Int(red, green, blue) {
+  return ((red & 0xff) << 16) + ((green & 0xff) << 8) + (blue & 0xff);
+}
+
+function write_lights(red, green, blue) {
+  // console.log('about to write', red, green, blue);
+  for (let i = 0; i < NUM_LEDS; i++) {
+    pixelData[i] = rgb2Int(red, green, blue);
+  }
+  ws281x.render(pixelData);
+}
+
+const oscServer = new osc.Server(1337, '0.0.0.0');
+
+oscServer.on("message", function (msg, rinfo) {
+  // console.log('message!', msg);
+  if (msg[0] === '/r') {
+    r_destination = parseInt(Number(msg[1]) * 255);
+  } else if (msg[0] === '/g') {
+    g_destination = parseInt(Number(msg[1]) * 255);
+  } else if (msg[0] === '/b') {
+    b_destination = parseInt(Number(msg[1]) * 255);
+  }
+});
+
